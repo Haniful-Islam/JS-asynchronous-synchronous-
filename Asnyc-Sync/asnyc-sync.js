c3552ef243b27a976c1adf8b9fc39b6e7c04f44c
Haniful Islam <haniful360@gmail.com>
2022-11-02 10:24:18 +0600

    js anynchornous and synchronous

---
+++ Asnyc-Sync/asnyc-sync.js
@@ -0,0 +1,27 @@
+
+const taskOne = () => {
+    console.log('task one');
+}//syc
+
+
+const taskTwo = () => {
+    console.log('task two');
+}
+
+const taskThree = () => {
+    console.log('task three');
+}
+
+const taskFour = () => {
+    console.log('task four');
+}
+
+const taskFive = () => {
+    console.log('task five');
+}
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
